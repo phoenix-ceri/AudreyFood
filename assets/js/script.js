01bdd3ddcf2d25d7520390b0b47abea6530bd463
GeckoGiggles <eva.judith.perez@gmail.com>
2022-12-22 22:50:13 -0500

    changed the logic JS and added some logic to access our API

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,9 @@
 var currentDate = moment().format('LLLL');
 $('#date').text(currentDate);
 
-const apikey = "51c9b31a24364a49b55fcecffd8cb687"
+const apiKey = "51c9b31a24364a49b55fcecffd8cb687"
+
+var restLonLatArray = [];
 
 //global variables
 var lonLat = "";
@@ -24,7 +26,8 @@ var requestOptions = {
   const getThings = async () => {
   const res = await fetch("./assets/JSON/restaurants.JSON", restaurantRequestOptions);
   const places = res.json();
-  console.log(places);
+  console.log(places + "This is the GetThings Function");
+  return places;
   };
 
   getThings();
@@ -48,17 +51,17 @@ console.log("Latitude: " + position.coords.latitude +
 }
 
 
-  //click function for submit button with user's criteria
-  var searchButton = document.getElementById("search-btn");
-  searchButton.addEventListener("click", function(){
+  // //click function for submit button with user's criteria
+  // var searchButton = document.getElementById("search-btn");
+  // searchButton.addEventListener("click", function(){
 
-  });
+  // });
 
   //function to plot user location marker on map
 
   
   //GeoApify Places API fetch 
-  fetch(`https://api.geoapify.com/v1/geocode/autocomplete?text=Mosco&apiKey=${apikey}`, requestOptions)
+  fetch(`https://api.geoapify.com/v1/geocode/autocomplete?text=Mosco&apiKey=${apiKey}`, requestOptions)
     .then(response => response.json())
     .then(result => console.log(result))
     .catch(error => console.log('error', error));
@@ -67,50 +70,61 @@ console.log("Latitude: " + position.coords.latitude +
     // Logic for Map tool from Geoapify
     
     //variable for static map, before user input, centered on Central Florida
-    var staticMapUrl = `https://maps.geoapify.com/v1/staticmap?style=osm-liberty&width=571&height=473&center=lonlat:-81.421539,28.641699&zoom=9.5577&marker=lonlat:-122.307060209015,47.526340765976215;color:%23ff0000;size:medium&apiKey=${apikey}`;
+    var staticMapUrl = `https://maps.geoapify.com/v1/staticmap?style=osm-liberty&width=571&height=473&center=lonlat:-81.421539,28.641699&zoom=9.5577&marker=lonlat:-122.307060209015,47.526340765976215;color:%23ff0000;size:medium&apiKey=${apiKey}`;
     
     //variable for markers 
-    var barMarkerUrl = `https://maps.geoapify.com/v1/staticmap?apiKey=${apikey}&style=osm-liberty&marker=lonlat:${lonLat};type:awesome;color:%23a82ecc;size:large;icon:glass-martini`;
+    var barMarkerUrl = `https://maps.geoapify.com/v1/staticmap?apiKey=${apiKey}&style=osm-liberty&marker=lonlat:${lonLat};type:awesome;color:%23a82ecc;size:large;icon:glass-martini`;
     
-    var restaurantMarkerUrl = `https://maps.geoapify.com/v1/staticmap?apiKey=${apikey}&style=osm-liberty&marker=lonlat:${lonLat};type:awesome;color:%23da291c;size:large;icon:utensils`;
+    var restaurantMarkerUrl = `https://maps.geoapify.com/v1/staticmap?apiKey=${apiKey}&style=osm-liberty&marker=lonlat:${lonLat};type:awesome;color:%23da291c;size:large;icon:utensils`;
    
-    var coffeeMarkerUrl = `https://maps.geoapify.com/v1/staticmap?apiKey=${apikey}&style=osm-liberty&marker=lonlat:${lonLat};type:awesome;color:%2300704a;size:large;icon:coffee`;   
+    var coffeeMarkerUrl = `https://maps.geoapify.com/v1/staticmap?apiKey=${apiKey}&style=osm-liberty&marker=lonlat:${lonLat};type:awesome;color:%2300704a;size:large;icon:coffee`;   
    
-    var userMarker = `https://maps.geoapify.com/v1/staticmap?apiKey=${apikey}&style=osm-liberty&marker=lonlat:${lonLat};type:awesome;color:%2317f4f9;icon:hippo`
+    var userMarker = `https://maps.geoapify.com/v1/staticmap?apiKey=${apiKey}&style=osm-liberty&marker=lonlat:${lonLat};type:awesome;color:%2317f4f9;icon:hippo`
 
-    queryParamsLink = `https://maps.geoapify.com/v1/staticmap?apiKey=${apikey}&style=osm-liberty&marker=lonlat:${lonLat};type:awesome;color:%2317f4f9;icon:${iconVar}`
+    queryParamsLink = `https://maps.geoapify.com/v1/staticmap?apiKey=${apiKey}&style=osm-liberty&marker=lonlat:${lonLat};type:awesome;color:%2317f4f9;icon:${iconVar}`
     var iconVar = 
 
   function createLink() {
-    link = `https://maps.geoapify.com/v1/staticmap?apiKey=${apikey}&style=osm-liberty&marker=lonlat:${lonLat};type:awesome;color:%2317f4f9;icon:${iconVar}`;
+    link = `https://maps.geoapify.com/v1/staticmap?apiKey=${apiKey}&style=osm-liberty&marker=lonlat:${lonLat};type:awesome;color:%2317f4f9;icon:${iconVar}`;
 
     array.foreach(link += "marker=lonlat:" + array.lonlat + ";type:awesome;color:%2317f4f9;icon:" + iconVar)
     //for loop probably
   }
+  placesURLEndpoint= `https://api.geoapify.com/v2/places?categories=catering&filter=rect:11.573106549898483,48.13898913611139,11.57704581350751,48.13666585409989&limit=20&apiKey=51c9b31a24364a49b55fcecffd8cb687`;
+// https://api.geoapify.com/v2/places?categories=catering&conditions=vegan&filter=rect:-81.421539,28.641699&limit=20&apiKey=${apiKey}
   fetch(placesURLEndpoint, requestOptions)
   .then(response => response.json())
   .then(result => console.log(result))
  .catch(error => console.log('error', error));
- placesURLEndpoint= `https://api.geoapify.com/v2/places?categories=${businesstype}&conditions=${conditions}&filter=rect:-81.421539,28.641699&limit=20&apiKey=${apikey}`;
 
- fetch(placesURLEndpoint).then(function (response) {
-  return response.json() //conversion of raw data
-}).then(function (data) {
-  renderFiveDayResults(data);
-  fetchCurrentWeather(data.city.coord.lat, data.city.coord.lon);
-  renderHistory();
-})
-renderMapQueryResults(data);
+renderMapQueryResults();
 showPosition(position.coords.longitude,position.coords.latitude);
 renderFilteredResults();
 
 function renderMapQueryResults(resultObj){
-  for (let i = 0; i < resultObj.length; i++) {
-  
-}
+    console.log ("function is running")
+  var data = async() => {
+    var data2 = await getThings();
+    for (var i = 0; i < data2.restaurants.length; i++) {
+      console.log (JSON.stringify(data2.restaurants[i].lon) + "," + JSON.stringify(data2.restaurants[i].lat) +" this is restaurant" + i);
+      restLonLatArray.push(JSON.stringify(data2.restaurants[i].lon) + "," + JSON.stringify(data2.restaurants[i].lat))
+    }
+    console.log(restLonLatArray + "this is the long lat array")
+    return data2;
+  // var maybethiswillwork = data();
+  // console.log(maybethiswillwork + "hopes and dreams")
+  // var queryRestruarantsLonLat = ""
+  // var queryRestruarantsLonLat += data.restaurants.lon + ',' + data.restaurants.lat
+  // console.log (data() + "this is text")
+  }
+  data()
+  mapString=""
+
+  document.getElementById('map-container').innerHTML = mapString+= `<img id="map" width="571" height="473" 
+    src="https://maps.geoapify.com/v1/staticmap?style=osm-liberty&width=571&height=473&center=lonlat:-81.421539,28.641699&zoom=9.5577&marker=lonlat:${queryRestruarantsLonLat};color:%23ff0000;size:medium&apiKey=${apiKey}"
+    alt="${features[i].properties.name}">`
 }
 
-var apiKey= "51c9b31a24364a49b55fcecffd8cb687"
 //as a last resort, add all restaurants, coffee shops, bars to their respective links...:(
 
     //Function to show results location on map
